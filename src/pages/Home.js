74418ed3f97fe5c7c8efbe67dbf05d3a4ff60683
Sikach Sergii <serjsv87@gmail.com>
2023-11-13 14:29:32 +0100

    add axios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,35 @@
 import { Center, Heading, Stack, Text } from "@chakra-ui/react";
 import GithubCorner from "components/GtihubCorner";
+import { useState, useEffect } from "react";
 
 const HomePage = () => {
-    return (
-        <Center h={'100vh'}>
-            <Stack>
-                <Heading>React + Chakra UI</Heading>
-                <Text>Create React App. Boilerplate. Docker.</Text>
-            </Stack>
+  const [backendData, setBackendData] = useState('');
 
-            <GithubCorner />
-        </Center>
-    )
-}
+  useEffect(() => {
+    fetchDataFromBackend();
+  }, []);
+
+  const fetchDataFromBackend = async () => {
+    try {
+      const response = await fetch('/api/'); // Замените '/api/' на реальный путь к вашему API на бэкенде
+      const data = await response.text();
+      setBackendData(data);
+    } catch (error) {
+      console.error('Error fetching data from backend:', error);
+    }
+  };
+
+  return (
+    <Center h={'100vh'}>
+      <Stack>
+        <Heading>React + Chakra UI</Heading>
+        <Text>Create React App. Boilerplate. Docker.</Text>
+        <Text>Data from Backend: {backendData}</Text>
+      </Stack>
+
+      <GithubCorner />
+    </Center>
+  );
+};
 
 export default HomePage;
